972d390f28dba794d3bc948761527869dfed019e
CamiMeneses <CamiMeneses@github.com>
2020-10-17 01:03:09 -0500

    Showing stations in Miami

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import './App.css'
+import bikeIcon from './bicycle.png'
+
+import L from 'leaflet'; // To use leaflet https://leafletjs.com/
 
 class App extends Component {
   constructor() {
@@ -9,29 +13,84 @@ class App extends Component {
     this.state = {
       response: false,
       endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      lat: 25.761681, // Changing location to MIAMI
+      lng: -80.191788,
+      zoom: 13,
+      dataTime: []
     };
 
   }
   componentDidMount() {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-   
+    socket.on('ApiData', data => { // Getting the data from the server
+      this.setState({
+        response: data,
+      }); // Set the response
+    });
   }
-  render() {
-    const { response } = this.state;
-    const position = [this.state.lat, this.state.lng]
-    return (
 
+  spots = () => {
+    var icon = L.icon({
+      iconUrl: bikeIcon,
+      iconSize: [30, 30]
+    });
+    if (this.state.response){
+      var dataTime = this.state.dataTime
+      dataTime.push(this.state.response)
+      console.log(dataTime);
+      console.log(dataTime.length)
+      if (dataTime.length >= 10){
+        console.log(dataTime.length)
+        dataTime.shift()
+      }
+    }
+
+    const miamiBikes = this.state;
+    console.log("hellloooo");
+
+    if(miamiBikes.response){
+    const stationsData = miamiBikes.response.network.stations // searching JSON network > stations
+
+    var places = stationsData.map(
+      (station, index) => { //station has theses keys: empty_slots, extra{address, uid}, free_bikes, id, latitude, longitude, name, timestamp
+        return (
+          <div>
+            <Marker position={[station.latitude, station.longitude]} icon={icon}>
+              <Popup>
+                <div className="address">
+                  {station.extra.address}
+                </div>
+                <div className="description">
+                  <hr />
+                  <b>Free bikes:</b> {station.free_bikes}
+                  <br />
+                  <b>Empty slots:</b> {station.empty_slots}
+                </div>
+              </Popup>
+            </Marker>
+          </div>
+        );
+      },
+    );
+    return places;
+  }
+  }
+
+  render() {
+    const miamiBikes = this.state;
+
+    return (
       <div className="map">
         <h1> City Bikes in Miami </h1>
-        <Map center={position} zoom={this.state.zoom}>
+        <Map center={[miamiBikes.lat, miamiBikes.lng]} zoom={miamiBikes.zoom}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          if(miamiBikes.response){
+            this.spots()
+          }
         </Map>
       </div>
     );
--- citybike-server/server.js
+++ citybike-server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
 
-
+const axios = require('axios'); //  We have to require Axios to use it
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
@@ -15,17 +15,35 @@ app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server); // < Interesting!
 let interval;
+let timeout = 3000;
 
 io.on("connection", socket => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
   console.log('New connection ' + socketId + ' from ' + clientIp);
+
+  //---------------------- Checking free bikes
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => checkBikes(socket), timeout);
+  //----------------------
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
 
-
+//----------------------
+const checkBikes = async socket => {
+  try { // Trying to get the info from the api
+    const res = await axios.get(citybikeurl);
+    socket.emit('ApiData', res.data);
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+}
+//----------------------
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
