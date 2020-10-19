d2e8f631819ac03fce8a70a7ba2064a05dbe9736
CamiMeneses <CamiMeneses@github.com>
2020-10-19 16:26:47 -0500

    showing last updates

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
@@ -26,17 +28,27 @@ class App extends Component {
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
     socket.on('ApiData', data => {
-      this.setState({
-        response: data,
-      });
+      this.setState({response: data});
+      this.record();
     });
   }
 
-  // Record Data
+  // Recording Data
   record = () => {
     if (this.state.response){
       var dataTime = this.state.dataTime
-      dataTime.push(this.state.response)
+
+      var currentTimestamp = this.state.response.network.stations[0].timestamp
+      var lastRegister = this.state.dataTime[this.state.dataTime.length-1]
+
+      if (lastRegister){
+        var lastTimestamp = lastRegister.network.stations[0].timestamp
+        if ( currentTimestamp !=  lastTimestamp ){ // It will not record registers with same time
+          dataTime.push(this.state.response)
+        }
+      }else{
+        dataTime.push(this.state.response)
+      }
 
       if (dataTime.length >= 1000){ //Maximum: 1000 registers
         dataTime.shift()
@@ -44,15 +56,53 @@ class App extends Component {
     }
   }
 
-  setStationsData = (numRegis) =>{
-    var stations = ""
-    if(this.state.response){
-      console.log("sliderValue:" + this.state.sliderValue)
-      if (this.state.sliderValue === ""){
-        stations = this.state.response.network.stations // searching JSON network > stations
-      }else{
-        stations = this.getRecordStationsOnDate(numRegis) // Getting historical spots to show
+  checkChanges(){
+    if (this.state.response){
+      var lastRegister = this.state.dataTime[this.state.dataTime.length-1]
+      var currentData = this.state.response
+
+      if(lastRegister){
+        var data1stations = currentData.network.stations;
+        var data2stations = lastRegister.network.stations;
+        var differentFreebikes = []
+
+        var freebikes1 = {}
+        data1stations.forEach(station =>
+          {
+            freebikes1[station.name] = station.free_bikes
+          }
+        )
+
+        var freebikes2 = {}
+        data2stations.forEach(station =>
+          {
+            freebikes2[station.name] = station.free_bikes
+          }
+        )
+
+        for (var name in freebikes1){
+          if (freebikes1[name] == freebikes2[name]){
+          }else{
+            console.log(name)
+            console.log(freebikes1[name])
+            console.log(freebikes2[name])
+            console.log("diferentes")
+            differentFreebikes.push([name,freebikes1[name]])
+          }
+        }
       }
+      console.log("checkUpdates:" + differentFreebikes);
+      return differentFreebikes;
+    }
+  }
+
+  // Selecting between live or historical data
+  setStationsData = (numRegis) =>{
+    var stations = "";
+
+    if(this.state.response){
+      if (this.state.sliderValue !== ""){ stations = this.getRecordStationsOnDate(numRegis) }
+      if (!stations || ""){ stations = this.state.response.network.stations }
 
       return stations;
     }
@@ -60,8 +110,8 @@ class App extends Component {
 
   getRecordStationsOnDate = (numRegis) => {
     console.log("value " + this.state.sliderValue)
-    if (this.state.dataTime[this.state.sliderValue]){
-      var choosenRegisValue = this.state.dataTime[this.state.sliderValue].network.stations
+    if (this.state.dataTime[this.state.sliderValue-1]){
+      var choosenRegisValue = this.state.dataTime[this.state.sliderValue-1].network.stations
     }
     return choosenRegisValue;
   }
@@ -72,11 +122,11 @@ class App extends Component {
       var places = stationsData.map(
         (station, index) => { //station has theses keys: empty_slots, extra{address, uid}, free_bikes, id, latitude, longitude, name, timestamp
           return (
-            <div>
-              <Marker position={[station.latitude, station.longitude]} icon={icon}>
+            <div key={`divspot_${station.extra.uid}`}>
+              <Marker key={`marker_${station.extra.uid}`} position={[station.latitude, station.longitude]} icon={icon}>
                 <Popup>
-                  <div className="address">
-                    {station.extra.address}
+                  <div className="name">
+                    {station.name}
                   </div>
                   <div className="description">
                     <hr />
@@ -97,7 +147,6 @@ class App extends Component {
 
   handleOnChange = (e) => {
     this.setState({sliderValue: e.target.value})
-    console.log("value: " + e.target.value);
   }
 
   sliderText = (stationsData) => {
@@ -106,9 +155,8 @@ class App extends Component {
     if (stationsData){
       var StationsTimestamp = stationsData[0].timestamp
       var dateFormat = new Date(StationsTimestamp)
-      sliderDate = 'hora del registro:' + dateFormat.getHours() + ':' + dateFormat.getMinutes() + ':' + dateFormat.getSeconds();
+      sliderDate = dateFormat.getHours() + 'h' + dateFormat.getMinutes() + 'm' + dateFormat.getSeconds() + 's';
     }
-    console.log("sliderDate:" + sliderDate);
     return sliderDate;
   }
 
@@ -116,6 +164,7 @@ class App extends Component {
     var numRegis = this.state.dataTime.length;
     var stationsData = this.setStationsData(numRegis);
     var sliderText = this.sliderText(stationsData);
+    var checkUpdates =  this.checkChanges();
 
     var icon = L.icon({
       iconUrl: bikeIcon,
@@ -130,18 +179,30 @@ class App extends Component {
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          if (this.state.response){[this.showSpots(icon, stationsData), this.record()]}
+          if (this.state.response){this.showSpots(icon, stationsData)}
         </Map>
+
+        <div className= 'updates'>
+          <h2>Last updates</h2>
+          <p>{checkUpdates}</p>
+        </div>
+
         <div className="register" id="register">
-        <div className="numRegis" style={{color: "#00ffff"}}>
-          Número de registros: {numRegis} <br />
-          Espera para ver el cambio en los registros. Actualizamos el registro cada 10 segundos, <br />
-          sin embargo, City Bike no cambia en un promedio de 5 minutos.
+          <div className="numRegis" style={{color: "#00ffff"}}>
+            <p>
+              Número de registros: {numRegis} <br />
+              Espera para ver el cambio en los registros. Actualizamos el registro cada 10 segundos, <br />
+              sin embargo, City Bike no cambia en un promedio de 5 minutos.
+            </p>
+          </div>
         </div>
-        </div>
-        <div >
+        <div>
           <input type="range" min={0} max={numRegis} value={this.state.sliderValue} className="slider" onChange={this.handleOnChange}/>
-          <div className="value" style={{color: "#00ffff"}}>{sliderText}</div>
+          <div className="value" style={{color: "#00ffff"}}>
+            <p>
+              <b>Registro número:</b> {this.state.sliderValue} <b>hora:</b> {sliderText}
+            </p>
+          </div>
         </div>
       </div>
     );
